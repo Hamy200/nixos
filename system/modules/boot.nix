9a8aec9853c6236c7155c367849f0bea641fb3f7
Hamy200 <104378548+Hamy200@users.noreply.github.com>
2025-06-29 23:14:35 +0100

    Update boot.nix to support luks

--- system/modules/boot.nix
+++ system/modules/boot.nix
@@ -1,9 +1,30 @@
 {config, lib, pkgs, inputs, ...}:
 {
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.useOSProber = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.initrd.kernelModules = ["amdgpu"];
+  boot = {
+    loader = {
+      grub = {
+        enable = true;
+        device = "nodev";
+        efiSupport = true;
+        useOSProber = true;
+        enableCryptodisk = true;
+      };
+
+      efi.canTouchEfiVariables = true;
+    };
+
+    initrd = {
+      kernelModules = ["amdgpu"];
+      luks.devices = {
+        crypt = {
+          device = "/dev/disk/by-label/crypt-container";
+          preLMV = true;
+        };
+      };
+    };
+
+    kernelPackages = pkgs.linuxPackages_latest;
+    
+  };
+  
 }
